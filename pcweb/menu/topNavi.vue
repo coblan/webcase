3c1c379398966cacacc000fe206d2fe28c584f99
coblan <coblan@163.com>
2022-02-09 01:34:10 +0800

    auto commit

---
+++ pcweb/menu/topNavi.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="top-navi">
+      <div class="left">
+
+      </div>
+        hello
+      <div class="right">
+        <slot name="right"></slot>
+      </div>
+
+    </div>
+</template>
+<script>
+export default {
+
+}
+</script>
+<style scoped lang="scss">
+.top-navi{
+  background-color: black;
+  color: white;
+  display: flex;
+  justify-content: space-between;
+}
+.right{
+
+}
+</style>
