9a1054af965dc039e9ff7cf1d49fb59423d477ff
coblan <coblan@163.com>
2022-06-08 00:54:43 +0800

    auto commit

---
+++ chart/sortBar.vue
@@ -0,0 +1,81 @@
+<template>
+     <echartsDiv ref="chart" ></echartsDiv>
+</template>
+
+<script>
+import  echartsDiv from 'weblib/uis/echartsDiv.vue'
+export default{
+    components:{
+        echartsDiv,
+    },
+    props:{
+        title:{},
+        ydata:{}, //['Brazil', 'Indonesia', 'USA', 'India', 'China', 'World']
+        xdata:{} 
+        /* [
+                        {
+                        name: '2011',
+                
+                        data: [18203, 23489, 29034, 104970, 131744, 630230]
+                        },]
+                        */
+    
+
+    },
+    computed:{
+        myoption(){
+            var series_data = []
+            this.xdata.forEach(item=>{
+                series_data.push({
+                    name:item.name,
+                    data:item.data,
+                    type:'bar',
+                    barWidth:20,
+                })
+            })
+           var option = {
+                    title: {
+                        text: this.title,
+                        left: 'center',
+                        top:'5%'
+                    },
+                    tooltip: {
+                        trigger: 'axis',
+                        axisPointer: {
+                        type: 'shadow'
+                        }
+                    },
+                    legend: {
+                         top:  'bottom',
+                         left: 'center',
+                    },
+                    grid: {
+                        left: '10%',
+                        right: '10%',
+                        bottom: '10%',
+                        containLabel: true
+                    },
+                    xAxis: {
+                        type: 'value',
+                        boundaryGap: [0, 0.01]
+                    },
+                    yAxis: {
+                        type: 'category',
+                        data: this.ydata
+                    },
+                    series: series_data
+                    };
+            return option
+        }
+    },
+    mounted(){
+        this.$nextTick(()=>{
+            this.$refs.chart.draw(this.myoption)  
+        })
+         
+    }
+}
+ 
+
+ 
+</script>
