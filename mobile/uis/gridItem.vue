4f225e04671c9e8e3a37afed7ed6c6d010f0d176
coblan <coblan@163.com>
2022-10-19 01:35:21 +0800

    auto commit

--- mobile/uis/gridItem.vue
+++ mobile/uis/gridItem.vue
@@ -1,8 +1,11 @@
 <template>
-    <div>
+    <div class="grid-item">
         <div class="myrow" v-for="head in heads" :key="head.name">
             <div class="my-label">{{head.label}}</div>
-            <div>{{row[head.name]}}</div>
+            <div v-if="head.editor=='com-table-picture'">
+              <img class="image-show" :src="row[head.name]" alt="">
+            </div>
+            <div v-else>{{row[head.name]}}</div>
         </div>
     </div>
 </template>
@@ -15,10 +18,21 @@ export default{
 }
 </script>
 <style scoped lang="scss">
+.grid-item{
+  margin: 10px 5px;
+  padding: 10px;
+  border: 1px solid #e1e1e1;
+  border-radius: 5px;
+}
 .myrow{
     display: flex;
+    align-items: center;
+    padding: 5px 0;
     .my-label{
         width: 100px;
     }
+}
+.image-show{
+  width: 100px;
 }
 </style>
