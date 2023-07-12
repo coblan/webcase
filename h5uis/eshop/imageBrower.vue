a5029aa0defe9173d98b7a5a529ecd615d53cd40
coblanplus <coblanplus@gmail.com>
2023-07-12 18:33:25 +0800

    auto commit

---
+++ h5uis/eshop/imageBrower.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <div v-for="src in images">
+            <imageDiv :src="src" alt=""></imageDiv>
+        </div>
+
+    </div>
+</template>
+<script>
+import imageDiv from 'weblib/uis/imageDiv.vue'
+export default {
+    components:{
+        imageDiv,
+    },
+    props:{
+        images:{}
+    },
+    data(){
+        return {
+            current_image:''
+        }
+    },
+    methods:{
+
+    }
+}
+</script>
